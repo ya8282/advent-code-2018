71078a3f51af979ceabb9b011f8047ca80b4f768
Christopher Cho <ccho@ChristohersiMac.fios-router.home>
2018-12-01 02:41:02 -0500

    Day 1 part 2 solution

---
+++ 1/first_dup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Determine the first instance of a duplicate frequency value
+import sys
+
+frequency = 0
+seen_set = set()
+
+# store file contents as parsed integer List
+with open('input.txt') as f:
+    seq = [ int(line.replace('\+','')) for line in f ]
+
+found = False
+while not found:
+    for i in seq:
+        frequency += i
+        if frequency in seen_set:
+            print "First duplicate: ", frequency
+            found = True
+            break
+        else:
+            seen_set.add(frequency)
+
